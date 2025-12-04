d616075654e8754098fe89fda94a49b15ed825df
Fedor Kutergin <fedornigretuk@gmail.com>
2025-12-05 02:33:29 +0300

    fix sliding window

--- src/main/kotlin/ru/quipy/common/utils/SlidingWindowRateLimiter.kt
+++ src/main/kotlin/ru/quipy/common/utils/SlidingWindowRateLimiter.kt
@@ -1,67 +1,78 @@
 package ru.quipy.common.utils
 
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.asCoroutineDispatcher
 import kotlinx.coroutines.delay
-import kotlinx.coroutines.sync.Mutex
-import kotlinx.coroutines.sync.withLock
+import kotlinx.coroutines.launch
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import java.time.Duration
-import kotlin.math.max
-
+import java.util.concurrent.Executors
+import java.util.concurrent.PriorityBlockingQueue
+import java.util.concurrent.atomic.AtomicLong
+import java.util.concurrent.locks.ReentrantLock
+import kotlin.concurrent.withLock
 
 class SlidingWindowRateLimiter(
     private val rate: Long,
-    window: Duration,
-) {
-    private val windowMillis = window.toMillis()
-    private val mutex = Mutex()
+    private val window: Duration,
+) : RateLimiter {
+    private val rateLimiterScope = CoroutineScope(Executors.newSingleThreadExecutor().asCoroutineDispatcher())
 
+    private val sum = AtomicLong(0)
+    private val queue = PriorityBlockingQueue<Measure>(10_000)
 
-    private val timestamps = ArrayDeque<Long>()
-
-    fun tryAcquire(): Boolean {
-        val now = System.currentTimeMillis()
-        return synchronized(this) {
-            evictExpired(now)
-            if (timestamps.size < rate) {
-                timestamps.addLast(now)
-                true
-            } else {
-                false
+    override fun tick(): Boolean {
+        while (true) {
+            val curSum = sum.get()
+            if (curSum >= rate) return false
+            if (sum.compareAndSet(curSum, curSum + 1)) {
+                queue.add(Measure(1, System.currentTimeMillis()))
+                return true
             }
         }
+    }
+
+    fun tickBlocking() {
+        while (!tick()) {
+            Thread.sleep(10)
+        }
+    }
+
+    suspend fun tickCoro() {
+        while (!tick()) { delay(10L) }
     }
 
     suspend fun acquireAsync() {
-        while (true) {
+        tickCoro()
+    }
 
-            val waitMillis = mutex.withLock {
-                val now = System.currentTimeMillis()
-                evictExpired(now)
-
-
-                if (timestamps.size < rate) {
-                    timestamps.addLast(now)
-                    return
-                }
-
-
-                val oldest = timestamps.first()
-                val elapsed = now - oldest
-                val remaining = windowMillis - elapsed
-                max(remaining, 0L)
-            }
-
-            if (waitMillis <= 0L) {
-                continue
-            }
-
-            delay(waitMillis)
+    data class Measure(
+        val value: Long,
+        val timestamp: Long
+    ) : Comparable<Measure> {
+        override fun compareTo(other: Measure): Int {
+            return timestamp.compareTo(other.timestamp)
         }
     }
 
-    private fun evictExpired(now: Long) {
-        val threshold = now - windowMillis
-        while (timestamps.isNotEmpty() && timestamps.first() <= threshold) {
-            timestamps.removeFirst()
+    private val releaseJob = rateLimiterScope.launch {
+        while (true) {
+            val head = queue.peek()
+            val winStart = System.currentTimeMillis() - window.toMillis()
+            if (head == null) {
+                delay(1L)
+                continue
+            }
+            if (head.timestamp > winStart) {
+                delay(head.timestamp - winStart)
+                continue
+            }
+            sum.addAndGet(-1)
+            queue.take()
         }
+    }.invokeOnCompletion { th -> if (th != null) logger.error("Rate limiter release job completed", th) }
+    companion object {
+        private val logger: Logger = LoggerFactory.getLogger(SlidingWindowRateLimiter::class.java)
     }
 }
