19568492ceb70c3aaffcaf7533131f83a92d9e85
Fedor Kutergin <fedornigretuk@gmail.com>
2026-02-27 21:18:46 +0300

    fix bucket

--- src/main/kotlin/ru/quipy/common/utils/LeakingBucketRateLimiterFactory.kt
+++ src/main/kotlin/ru/quipy/common/utils/LeakingBucketRateLimiterFactory.kt
@@ -50,12 +50,12 @@ class LeakingBucketRateLimiterFactory : RateLimiterFactory {
 
         val effectiveRps = min(
             account.rateLimitPerSec().toDouble(),
-            account.parallelRequests().toDouble() / totalProcessingTime.seconds
+            account.parallelRequests().toDouble() / totalProcessingTime.toMillis() * 1000
         )
 
         logger.info("Effective RPS: $effectiveRps")
 
-        return max(1, (effectiveRps * processingWaitLimit.seconds).toInt())
+        return max(1, (effectiveRps * processingWaitLimit.toMillis() / 1000).toInt())
     }
 
     companion object {
