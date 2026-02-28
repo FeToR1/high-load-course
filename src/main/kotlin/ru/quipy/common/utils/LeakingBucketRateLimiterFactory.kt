d1fe169ea86010941ce2afb5675d7bafc62d5f51
MaksOn <maksimchernyaev22@gmail.com>
2026-02-28 23:42:21 +0300

    app proc time up

--- src/main/kotlin/ru/quipy/common/utils/LeakingBucketRateLimiterFactory.kt
+++ src/main/kotlin/ru/quipy/common/utils/LeakingBucketRateLimiterFactory.kt
@@ -58,6 +58,6 @@ class LeakingBucketRateLimiterFactory : RateLimiterFactory {
     companion object {
         private const val DEFAULT_BUCKET_SIZE = 100
         private val logger = LoggerFactory.getLogger(LeakingBucketRateLimiterFactory::class.java)
-        private val APP_PROCESSING_TIME = Duration.ofMillis(0)
+        private val APP_PROCESSING_TIME = Duration.ofMillis(350)
     }
 }
