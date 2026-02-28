863f6adfca6cda4d2a40ec2b056fe05205ff79db
MaksOn <maksimchernyaev22@gmail.com>
2026-02-28 23:10:08 +0300

    fix

--- src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
+++ src/main/kotlin/ru/quipy/payments/config/PaymentAccountsConfig.kt
@@ -17,7 +17,6 @@ import ru.quipy.payments.api.PaymentAggregate
 import ru.quipy.payments.logic.PaymentAccountProperties
 import ru.quipy.payments.logic.PaymentAggregateState
 import ru.quipy.payments.logic.PaymentExternalSystemAdapter
-import ru.quipy.payments.logic.PaymentExternalSystemAdapterImpl
 import java.net.URI
 import java.net.http.HttpClient
 import java.net.http.HttpRequest
@@ -69,7 +68,7 @@ class PaymentAccountsConfig {
             .map { it.copy(enabled = true) }
             .onEach(::println)
             .map {
-                PaymentExternalSystemAdapterImpl(
+                PaymentExternalSystemAdapter(
                     it,
                     paymentService,
                     paymentProviderHostPort,
