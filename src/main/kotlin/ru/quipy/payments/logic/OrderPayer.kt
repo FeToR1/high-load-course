39bed23667dce2b50c962de319098ca7267a6d59
MaksOn <maksimchernyaev22@gmail.com>
2026-03-01 02:45:16 +0300

    set queue size to 100

--- src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
+++ src/main/kotlin/ru/quipy/payments/logic/OrderPayer.kt
@@ -35,7 +35,7 @@ class OrderPayer(
         THREAD_POOL_SIZE,
         0,
         TimeUnit.SECONDS,
-        LinkedBlockingQueue(4000),
+        LinkedBlockingQueue(100),
         NamedThreadFactory("payment-submission-executor"),
         CallerBlockingRejectedExecutionHandler()
     )
