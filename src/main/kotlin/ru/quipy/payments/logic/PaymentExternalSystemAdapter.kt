f41e5af476562213d2287736e23a862ed2f570fa
MaksOn <maksimchernyaev22@gmail.com>
2026-03-02 00:04:03 +0300

    add timeout

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapter.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapter.kt
@@ -10,15 +10,14 @@ import kotlinx.coroutines.time.delay
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import ru.quipy.common.utils.OngoingWindow
+import ru.quipy.common.utils.RateLimitExceededException
 import ru.quipy.common.utils.SlidingWindowRateLimiter
 import ru.quipy.core.EventSourcingService
 import ru.quipy.monitoring.MonitoringService
 import ru.quipy.monitoring.RequestType
 import ru.quipy.payments.api.PaymentAggregate
 import java.net.URI
-import java.net.http.HttpClient
-import java.net.http.HttpRequest
-import java.net.http.HttpResponse
+import java.net.http.*
 import java.time.Duration
 import java.time.Instant
 import java.util.*
@@ -50,6 +49,7 @@ class PaymentExternalSystemAdapter(
     private val client: HttpClient by lazy {
         HttpClient.newBuilder()
             .version(HttpClient.Version.HTTP_2)
+            .connectTimeout(Duration.ofMillis(100))
             .build()
     }
 
@@ -64,7 +64,7 @@ class PaymentExternalSystemAdapter(
         val request = HttpRequest.newBuilder()
             .uri(URI.create("http://$paymentProviderHostPort/external/process?serviceName=${properties.serviceName}&token=$token&accountName=${properties.accountName}&transactionId=$transactionId&paymentId=$paymentId&amount=$amount"))
             .POST(HttpRequest.BodyPublishers.noBody())
-            .timeout(Duration.ofSeconds(40))
+            .timeout(monitoringService.get90thPercentileTimeout(properties.accountName))
             .build()
 
         sendRequest(request, paymentId, transactionId, deadline)
@@ -104,37 +104,57 @@ class PaymentExternalSystemAdapter(
                 rateLimiter.tickAsync()
                 ongoingWindow.acquireAsync()
                 val startTime = now()
-                val response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).await()
-                val duration = Duration.between(startTime, now()).toMillis()
+                try {
+                    val response = client.sendAsync(request, HttpResponse.BodyHandlers.ofString()).await()
+                    val duration = Duration.between(startTime, now()).toMillis()
 
-                val body = try {
-                    mapper.readValue(response.body(), ExternalSysResponse::class.java)
-                } catch (e: Exception) {
-                    logger.error("[$accountName] Failed to parse response for txId: $transactionId, payment: $paymentId, result code: ${response.statusCode()}, reason: ${response.body()}")
-                    ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
-                }
-
-                monitoringService.increaseRequestsCounter(RequestType.OUTGOING)
-                monitoringService.recordRequestDuration(duration, body.result)
-
-                if (response.statusCode() in 200..299) {
-                    scope.launch {
-                        paymentESService.update(paymentId) {
-                            it.logProcessing(body.result, now().toEpochMilli(), transactionId, reason = body.message)
-                        }
+                    val body = try {
+                        mapper.readValue(response.body(), ExternalSysResponse::class.java)
+                    } catch (e: Exception) {
+                        logger.error("[$accountName] Failed to parse response for txId: $transactionId, payment: $paymentId, result code: ︠{response.statusCode()}, reason: ︠{response.body()}")
+                        ExternalSysResponse(transactionId.toString(), paymentId.toString(), false, e.message)
                     }
-                    val requestType = if (body.result) RequestType.PROCESSED_SUCCESS else RequestType.PROCESSED_FAIL
-                    monitoringService.increaseRequestsCounter(requestType)
-                    return
+
+                    monitoringService.increaseRequestsCounter(RequestType.OUTGOING)
+                    monitoringService.recordRequestDuration(duration, body.result)
+
+                    if (response.statusCode() in 200..299) {
+                        scope.launch {
+                            paymentESService.update(paymentId) {
+                                it.logProcessing(
+                                    body.result,
+                                    now().toEpochMilli(),
+                                    transactionId,
+                                    reason = body.message
+                                )
+                            }
+                        }
+                        val requestType = if (body.result) RequestType.PROCESSED_SUCCESS else RequestType.PROCESSED_FAIL
+                        monitoringService.increaseRequestsCounter(requestType)
+                        return
+                    }
+
+                    logger.warn("[$accountName] Non-success status ︠{response.statusCode()} for txId: $transactionId, attempt $i")
+                } catch (e: HttpTimeoutException) {
+                    logger.error(
+                        "[$accountName] Payment request timed out for txId: $transactionId, payment: $paymentId, attempt $i",
+                        e
+                    )
+                    throw RateLimitExceededException(
+                        monitoringService.get90thPercentileTimeout(properties.accountName).toMillis()
+                    )
+                } catch (e: HttpConnectTimeoutException) {
+                    logger.error(
+                        "[$accountName] Connection timed out for txId: $transactionId, payment: $paymentId, attempt $i",
+                        e
+                    )
+                } catch (e: Exception) {
+                    logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
                 }
-
-                logger.warn("[$accountName] Non-success status ${response.statusCode()} for txId: $transactionId, attempt $i")
-
-            } catch (e: Exception) {
-                logger.error("[$accountName] Payment failed for txId: $transactionId, payment: $paymentId", e)
             } finally {
                 ongoingWindow.release()
             }
+
         }
 
         logger.error("[${accountName}] All retry attempts exhausted for txId: $transactionId, payment: $paymentId")
