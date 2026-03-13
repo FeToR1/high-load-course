143544e1380b2b18b0026784de944876f6b10c28
Fedor Kutergin <fedornigretuk@gmail.com>
2026-03-14 00:46:46 +0300

    Revert "50 ms"
    This reverts commit 9f8cbcfc2522734da4a3f1d480d6761141cf97db.

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapter.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapter.kt
@@ -94,7 +94,7 @@ class PaymentExternalSystemAdapter(
                 monitoringService.increaseRetryCounter()
             }
 
-            if (now().plus(retryDelay) > deadline || Duration.between(now(), deadline).toMillis() < 50) {
+            if (now().plus(retryDelay) > deadline) {
                 logPaymentResult(paymentId, transactionId, false, "Deadline exceeded")
                 monitoringService.increaseRequestsCounter(RequestType.PROCESSED_FAIL)
                 return
