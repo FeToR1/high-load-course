53361c557382451dfbd1d0201b09c8f57e5ab526
Fedor Kutergin <fedornigretuk@gmail.com>
2026-03-18 18:20:20 +0300

    MAX_DELAY_MS 10L

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapter.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapter.kt
@@ -41,7 +41,7 @@ class PaymentExternalSystemAdapter(
 
         const val RETRY_DELAY_BASE = 2.0
         const val RETRY_DELAY_COEFF = 50
-        const val MAX_DELAY_MS = 10000000L
+        const val MAX_DELAY_MS = 10L
         const val MAX_RETRIES = 2
         const val MAX_ATTEMPTS = MAX_RETRIES + 1
     }
