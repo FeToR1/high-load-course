deeb178b311b6c7898bbdc84fa49ab65f8390b14
Fedor Kutergin <fedornigretuk@gmail.com>
2026-03-19 22:40:50 +0300

    timeout

--- src/main/kotlin/ru/quipy/monitoring/MonitoringService.kt
+++ src/main/kotlin/ru/quipy/monitoring/MonitoringService.kt
@@ -50,7 +50,7 @@ class MonitoringService {
 
         private val ACCOUNT_TIMEOUTS = mapOf(
             "acc-7" to 1070L,
-            "acc-13" to 500L,
+            "acc-13" to 900L,
         )
     }
 }
