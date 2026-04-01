6073c3d8b5bb61f43eddcb9ffa89d973e8108120
ziment <ziment33@gmail.com>
2026-04-02 00:56:03 +0300

    always log requests

--- src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapterImpl.kt
+++ src/main/kotlin/ru/quipy/payments/logic/PaymentExternalSystemAdapterImpl.kt
@@ -147,6 +147,18 @@ class PaymentExternalSystemAdapterImpl(
         fun isCircuitBreakerOpen() = circuitBreaker.state == CircuitBreaker.State.OPEN
 
         for (attempt in 1..MAX_ATTEMPTS) {
+            // Логируем каждую попытку отправки запроса
+            // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования
+            dbScope.launch {
+                paymentESService.update(paymentId) {
+                    it.logSubmission(
+                        success = true,
+                        transactionId,
+                        now().toEpochMilli(),
+                        Duration.ofMillis(now().toEpochMilli() - paymentStartedAt))
+                }
+            }
+
             if (isCircuitBreakerOpen()) {
                 break
             }
@@ -245,18 +257,6 @@ class PaymentExternalSystemAdapterImpl(
         if (!circuitBreaker.tryAcquirePermission()) {
             logger.warn("[$accountName] Circuit breaker is OPEN, rejecting request for payment $paymentId")
             return PaymentResult(success = false, paymentSucceeded = false, message = "Circuit breaker is open", shouldRetry = false)
-        }
-
-        // Логируем каждую попытку отправки запроса
-        // Это требуется сделать ВО ВСЕХ СЛУЧАЯХ, поскольку эта информация используется сервисом тестирования
-        dbScope.launch {
-            paymentESService.update(paymentId) {
-                it.logSubmission(
-                    success = true,
-                    transactionId,
-                    now().toEpochMilli(),
-                    Duration.ofMillis(now().toEpochMilli() - paymentStartedAt))
-            }
         }
 
         val startTime = now()
